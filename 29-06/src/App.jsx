11ba9c7fe1f1fa8aed65f91e39dd9e5a1e04f3f7
Heitor Schafer Lima <heitorslima2006@hotmail.com>
2026-06-30 21:50:38 -0300

    30-06
    UC12

--- 29-06/src/App.jsx
+++ 29-06/src/App.jsx
@@ -4,18 +4,12 @@ import Card from './components/Card';
 
 function App() {
 
-function mostrarMensagem(){
-  alert("botão 3 clicado");
-}
+
 
   return (
     <div>
       <h1>Snowy News</h1>
-      <Botao text={"Notícias"} background={"btn-primario"} func={() => alert("Botão 1 clicado")}/>
-      <Botao text={"Assinaturas"} background={"btn-secundario"} func={() => alert("Botão 2 clicado")}/>
-      <Botao text={"Sua conta"} background={"btn-desativado"} func={mostrarMensagem}/>
-
-      <Card foto={"https://www.webmotors.com.br/imagens/prod/346603/PORSCHE_911_4.0_24V_H6_GASOLINA_GT3_RS_PDK_34660316561720761.webp"} titulo={"Porsche 911 GT3 RS"} paragrafo={"é um carro muito rápido"} texto={"clique para concordar"}/>
+      <Botao text={"Clique"}/>
     </div>
   )
 }
--- 29-06/src/components/Botao.jsx
+++ 29-06/src/components/Botao.jsx
@@ -1,9 +1,9 @@
 // assim criamos um componente reutilizável
 
-function Botao({text, background, func}) {
+function Botao({text}) {
   return (
     // em JSX, não existe 'class' e sim 'className'
-    <button onClick={func} className={background}>{text}</button>
+    <button className="bg-green-700 px-2 py-1 rounded-lg text-white hover:bg-green-900 transition-all duration-400 cursor-pointer">{text}</button>
   )
 }
 
